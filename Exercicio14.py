e7a2b4b03387ae0dc9c2f9e1dff42fedef2885d6
Rhumenigue <rhumenigue@gmail.com>
2022-06-30 13:54:04 -0300

    Exercicio14

---
+++ Exercicio14.py
@@ -0,0 +1,7 @@
+def quantosSemestres(registro, ano_corrente, semestre_corrente):
+  ano = int(str(registro)[0:2]) + 2000
+  semestre = int(str(registro)[3])
+  print(((ano_corrente - ano) * 2) - semestre + semestre_corrente)def quantosSemestres(registro, ano_corrente, semestre_corrente):
+  ano = int(str(registro)[0:2]) + 2000
+  semestre = int(str(registro)[3])
+  print(((ano_corrente - ano) * 2) - semestre + semestre_corrente)
